29d20d0134058252d36f89939c2ff64057d0f456
GSAce <GS_Ace@hotmail.com>
2026-03-07 16:21:06 -0800

    added BuildLog.cs to repository

---
+++ CabinetOS.Core/Arcade/SetBuilder/BuildLog.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CabinetOS.Core.Arcade.SetBuilder
+{
+    // ============================================================
+    // BUILD LOG
+    // ============================================================
+    public class BuildLog
+    {
+        private readonly List<string> _entries = new();
+        public IReadOnlyList<string> Entries => _entries;
+
+        public void Add(string message)
+        {
+            lock (_entries)
+            {
+                _entries.Add($"[{DateTime.Now:HH:mm:ss}] {message}");
+            }
+        }
+    }
+}
